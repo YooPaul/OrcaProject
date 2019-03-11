bc350074d12f96ef597e9d217a481e08b20de3ff
Andrew Rudasics <rudasics@cs.washington.edu>
2019-03-11 13:21:28 -0700

    added path

---
+++ Assets/Oculus/FollowPath.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    [SerializeField]
+    public Transform player;
+    public float playerSpeed = 3.0f;
+    public float triggerDistance;
+    public List<Transform> waypoints;
+
+    private int current, target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = 0;
+        target = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (atTarget()) {
+            current = target;
+            if (target < waypoints.Count-1) {
+                target++;
+            }
+        }
+
+        Vector2 xy = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+        
+        Vector3 move = Vector3.zero;
+        if (Input.GetKey(KeyCode.UpArrow) || (Mathf.Sign(xy.y) > 0 && xy.y != 0.0f)) {
+            move = waypoints[target].position - player.position;
+        } else if (Input.GetKey(KeyCode.DownArrow) || (Mathf.Sign(xy.y) < 0 && xy.y != 0.0f)) {
+            move = waypoints[current].position - player.position;
+        }
+        player.position += move.normalized * playerSpeed * Time.deltaTime;
+    }
+
+    private bool atTarget() {
+        return ((player.position - waypoints[target].position).magnitude < triggerDistance);
+    }
+
+    
+}
---
+++ Assets/Oculus/WhalePath.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhalePath : MonoBehaviour {
+
+    public Animator anim;
+
+    [SerializeField]
+    public Transform player, whale;
+    public float moveSpeed = 3.0f;
+    public float triggerDistance, playerTrigger;
+    public List<Transform> waypoints;
+
+    private int current, target;
+    private bool reached, prev;
+
+    // Start is called before the first frame update
+    void Start() {
+        current = 0;
+        target = 1;
+        reached = true;
+        prev = true;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (atTarget()) {
+            reached = true;
+            if (reached != prev) {
+                anim.SetBool("Follow", true);
+            }
+
+            if ((player.position - whale.position).magnitude < playerTrigger) {
+                current = target;
+                if (target < waypoints.Count - 1) {
+                    target++;
+                }
+            }
+        }
+
+        Vector3 move = waypoints[target].position - whale.position;
+        whale.position += move.normalized * moveSpeed * Time.deltaTime;
+    }
+
+    private bool atTarget() {
+        return ((whale.position - waypoints[target].position).magnitude < triggerDistance);
+    }
+
+}
