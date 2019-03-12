5e5f53c3b259655f5320f5959900e29bd3ecc24e
Paul Yoo <yoosehy@cs.washington.edu>
2019-03-11 21:13:36 -0700

    Installed swimpoints and refined the state machine

--- Assets/Oculus/FollowPath.cs
+++ Assets/Oculus/FollowPath.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class FollowPath : MonoBehaviour
 {
     [SerializeField]
-    public Transform player;
+    public Transform player, whale;
     public float playerSpeed = 3.0f;
+    public float lookSpeed = 0.5f;
     public float triggerDistance;
     public List<Transform> waypoints;
 
@@ -22,28 +23,43 @@ public class FollowPath : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if (atTarget()) {
-            current = target;
-            if (target < waypoints.Count-1) {
-                target++;
-            }
-        }
+        var targetRotation = Quaternion.LookRotation(waypoints[target].position - transform.position);
 
-        Vector2 xy = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
-        
-        Vector3 move = Vector3.zero;
-        if (Input.GetKey(KeyCode.UpArrow) || (Mathf.Sign(xy.y) > 0 && xy.y != 0.0f)) {
-            move = waypoints[target].position - player.position;
-        } else if (Input.GetKey(KeyCode.DownArrow) || (Mathf.Sign(xy.y) < 0 && xy.y != 0.0f)) {
-            move = waypoints[current].position - player.position;
+        // Smoothly rotate towards the target point.
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, lookSpeed * Time.deltaTime);
+
+        if (!atTarget() && whale.GetComponent<WhalePath>().hasReached()) {
+            Vector2 xy = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+            Debug.Log(xy);
+            Vector3 move = Vector3.zero;
+            if (Input.GetKey(KeyCode.UpArrow) || (Mathf.Sign(xy.y) > 0 && xy.y != 0.0f))
+            {
+                move = waypoints[target].position - player.position;
+            }
+            else if (Input.GetKey(KeyCode.DownArrow) || (Mathf.Sign(xy.y) < 0 && xy.y != 0.0f))
+            {
+                move = waypoints[current].position - player.position;
+            }
+            player.position += move.normalized * playerSpeed * Time.deltaTime;
         }
-        player.position += move.normalized * playerSpeed * Time.deltaTime;
+        
     }
 
     private bool atTarget() {
         return ((player.position - waypoints[target].position).magnitude < triggerDistance);
     }
 
+    public void updateTarget()
+    {
+        current = target;
+        if (target < waypoints.Count - 1)
+        {
+            target++;
+        }
+        else if (target == waypoints.Count - 1)
+        {
+            target--;
+        }
+    }
     
 }
--- Assets/Oculus/WhalePath.cs
+++ Assets/Oculus/WhalePath.cs
@@ -7,62 +7,101 @@ public class WhalePath : MonoBehaviour {
     public Animator anim;
 
     [SerializeField]
-    public Transform player, whale;
+    public Transform player, whale, lookTarget;
     public float moveSpeed = 3.0f;
+    public float lookSpeed = 2.0f;
     public float triggerDistance, playerTrigger, celebrationTime;
     public List<Transform> waypoints;
 
 
-    private int current, target;
-    private bool reached, prev, celebrating;
-    private float celebrationStart = 0.0f;
+    public int current, target;
+    public bool reached, prev, playerReached, prevR, celebrating;
+    public float celebrationStart = 0.0f;
 
     // Start is called before the first frame update
     void Start() {
         current = 0;
         target = 1;
-        reached = true;
-        prev = true;
+        reached = false;
+        prev = false;
+        playerReached = false;
+        prevR = false;
+        lookTarget = player;
     }
 
     // Update is called once per frame
     void Update() {
 
+        var targetRotation = Quaternion.LookRotation(lookTarget.transform.position - transform.position);
+
+        // Smoothly rotate towards the target point.
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, lookSpeed * Time.deltaTime);
+
+        playerReached = (player.position - whale.position).magnitude < playerTrigger;
+        
+
+        if (target > 3)
+        {
+            playerTrigger = 15;
+        }
+
         if (atTarget()) {
             reached = true;
+           lookTarget = player;
             if (reached != prev) {
                 anim.SetBool("Follow", true);
+                anim.SetBool("Swim", false);
+                
                 prev = reached;
             }
 
-            if ((player.position - whale.position).magnitude < playerTrigger) {
+            
+            
+            if (playerReached && playerReached != prevR) {
                 celebrating = true;
                 anim.SetBool("Celebrating", true);
                 celebrationStart = Time.time;
+                prevR = playerReached;
+            } else if (!playerReached) {
+                prevR = false;
             }
-            if(Time.time - celebrationStart < celebrationTime != celebrating) {
-                celebrating = false;
-                anim.SetBool("Celebrating", false);
-                current = target;
-                if (target < waypoints.Count - 1) {
-                    target++;
-                }
-            }
-
-            
-
+            if(Time.time - celebrationStart > celebrationTime && celebrating) {
+                
+            } 
+        } else {
+            reached = false;
+            prev = false;
+            lookTarget = (waypoints[target]);
         }
 
-        if (celebrating) {
-            Debug.Log("Celebrting: " + celebrating);
+        if (anim.GetBool("Swim")) {
+            Vector3 move = waypoints[target].position - whale.position;
+            whale.position += move.normalized * moveSpeed * Time.deltaTime;
         }
-
-        Vector3 move = waypoints[target].position - whale.position;
-        whale.position += move.normalized * moveSpeed * Time.deltaTime;
+        
     }
 
     private bool atTarget() {
         return ((whale.position - waypoints[target].position).magnitude < triggerDistance);
     }
 
+    public void moveToNext()
+    {
+        celebrating = false;
+        anim.SetBool("Celebrating", false);
+        current = target;
+        if (target < waypoints.Count - 1)
+        {
+            target++;
+        }
+        anim.SetInteger("Target", target);
+        anim.SetBool("Swim", true);
+        anim.SetBool("Follow", false);
+        player.GetComponent<FollowPath>().updateTarget();
+    }
+
+    public bool hasReached()
+    {
+        return reached;
+    }
 }
